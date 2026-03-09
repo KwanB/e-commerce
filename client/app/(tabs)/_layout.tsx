885b10b8a8341ca726e923aed9bc1d1fcf6ad5c7
KwanB <horizonbot1@gmail.com>
2026-03-09 21:42:10 +0700

    Add Header component

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,23 +1,73 @@
-
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Feather, Ionicons } from '@expo/vector-icons'
+import { COLORS } from "@/assets/constants";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import React from "react";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-        <Tabs.Screen name='index' options={{
-            tabBarIcon: ({color, focused}) => <Ionicons name={focused ? "home" : "home-outline"} size={26} color={color}/>
-        }}/>
-        <Tabs.Screen name='cart' options={{
-            tabBarIcon: ({color, focused}) => <Feather name={focused ? "shopping-cart" : "shopping-cart"} size={26} color={color}/>
-        }}/>
-        <Tabs.Screen name='favorites' options={{
-            tabBarIcon: ({color, focused}) => <Ionicons name={focused ? "heart" : "heart-outline"} size={26} color={color}/>
-        }}/>
-        <Tabs.Screen name='profile' options={{
-            tabBarIcon: ({color, focused}) => <Ionicons name={focused ? "person" : "person-outline"} size={26} color={color}/>
-        }}/>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#ffff",
+          borderTopWidth: 1,
+          borderTopColor: "#F0F0F0",
+          height: 56,
+          paddingTop: 8,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={26}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Feather
+              name={focused ? "shopping-cart" : "shopping-cart"}
+              size={26}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "heart" : "heart-outline"}
+              size={26}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={26}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
-  )
+  );
 }
--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import Header from "@/components/Header";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    <SafeAreaView className="flex-1" edges={["top"]}>
+      <Header title="Forever" showMenu showCart showLogo />
+    </SafeAreaView>
+  );
 }
