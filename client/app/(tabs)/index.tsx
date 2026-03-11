4eb9458134682dcb729b01b3186cc0c7d3068dda
KwanB <horizonbot1@gmail.com>
2026-03-12 01:28:04 +0700

    fix(index-tabs): resolve issues in index component

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -68,6 +68,9 @@ export default function Home() {
                   className="w-full h-full"
                   resizeMode="cover"
                 />
+
+                <View className="absolute inset-0 bg-black/40" />
+
                 <View className="absolute bottom-4 left-4 z-10">
                   <Text className="text-white text-2xl font-bold">
                     {banner.title}
@@ -81,7 +84,6 @@ export default function Home() {
                     </Text>
                   </TouchableOpacity>
                 </View>
-                <View className="absolute inset-0 bg-black/40" />
               </View>
             ))}
           </ScrollView>
@@ -129,13 +131,28 @@ export default function Home() {
           {loading ? (
             <ActivityIndicator size="large" />
           ) : (
-            <View>
+            <View className="flex-row flex-wrap justify-between">
               {products.slice(0, 4).map((product) => (
                 // <Text key={products._id}>{products.name}</Text>
                 <ProductCard key={product._id} product={product} />
               ))}
             </View>
           )}
+        </View>
+
+        {/* Newselectter CTA  */}
+        <View className="bg-gray-100 p-6 rounded-2xl mb-20 items-center">
+          <Text className="text-2xl font-bold text-primary mb-2 text-center">
+            Join the Revolution
+          </Text>
+          <Text className="text-secondary text-center mb-4">
+            Subscibe to our newsletter and get 10% off your first purchase.
+          </Text>
+          <TouchableOpacity className="bg-primary w-4/5 py-3 rounded-full items-center">
+            <Text className="text-white font-medium text-base">
+              Subscribe Now
+            </Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
